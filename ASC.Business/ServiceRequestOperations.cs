a80d83c6d11a6b06cb8658487ab4dcdacb92efcb
Vinhtatoo0917 <Vinhtatoo0917@gmail.com>
2025-05-18 19:57:48 +0700

    full lab

---
+++ ASC.Business/ServiceRequestOperations.cs
@@ -0,0 +1,64 @@
+﻿using ASC.Business.Interfaces;
+using ASC.DataAccess;
+using ASC.DataAccess.Interfaces;
+using ASC.Model.Models;
+using ASC.Model.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC.Business
+{
+    public class ServiceRequestOperations : IServiceRequestOperations
+    {
+        private readonly IUnitOfWork _uniOfWork;
+
+        public ServiceRequestOperations(IUnitOfWork uniOfWork)
+        {
+            _uniOfWork = uniOfWork;
+        }
+        public async Task CreateServiceRequestAsync(ServiceRequest request)
+        {
+            using (_uniOfWork)
+            {
+                await _uniOfWork.Repository<ServiceRequest>().AddAsync(request);
+                _uniOfWork.CommitTransaction();
+
+            }
+        }
+
+        public ServiceRequest UpdateServiceRequest(ServiceRequest request)
+        {
+            using (_uniOfWork)
+            {
+                _uniOfWork.Repository<ServiceRequest>().Update(request);
+                _uniOfWork.CommitTransaction();
+                return request;
+            }
+        }
+
+        public async Task<ServiceRequest> UpdateServiceRequestStatusAsync(string rowKey, string partitionKey, string status)
+        {
+            using (_uniOfWork)
+            {
+                var serviceRequest = await _uniOfWork.Repository<ServiceRequest>().FindAsync(partitionKey, rowKey);
+                if (serviceRequest == null)
+                    throw new NullReferenceException();
+                serviceRequest.Status = status;
+                _uniOfWork.Repository<ServiceRequest>().Update(serviceRequest);
+                _uniOfWork.CommitTransaction();
+                return serviceRequest;
+            }
+        }
+
+        public async Task<List<ServiceRequest>> GetServiceRequestsByRequestedDateAndStatus(
+      DateTime? requestedDate, List<string> status = null, string email = "", string serviceEngineerEmail = "")
+        {
+            var query = Queries.GetDashboardQuery(requestedDate, status, email, serviceEngineerEmail);
+            var serviceRequests = await _uniOfWork.Repository<ServiceRequest>().FindAllByQuery(query);
+            return serviceRequests.ToList();
+        }
+    }
+}
---
+++ ASC.Models/Queries/Queries.cs
@@ -0,0 +1,53 @@
+﻿using ASC.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ASC.Utilities;
+
+namespace ASC.Model.Queries
+{
+    public static class Queries
+    {
+        public static Expression<Func<ServiceRequest, bool>> GetDashboardQuery(
+    DateTime? requestedDate,
+    List<string> status = null,
+    string email = "",
+    string serviceEngineerEmail = "")
+        {
+            var query = (Expression<Func<ServiceRequest, bool>>)(u => true);
+
+            if (requestedDate.HasValue)
+            {
+                query = query.And(u => u.RequestedDate >= requestedDate.Value);
+            }
+
+            if (!string.IsNullOrWhiteSpace(email))
+            {
+                query = query.And(u => u.PartitionKey == email);
+            }
+
+            if (!string.IsNullOrWhiteSpace(serviceEngineerEmail))
+            {
+                query = query.And(u => u.ServiceEngineer == serviceEngineerEmail);
+            }
+
+            if (status != null && status.Count > 0)
+            {
+                Expression<Func<ServiceRequest, bool>> statusQuery = u => false;
+                foreach (var state in status)
+                {
+                    string temp = state;
+                    statusQuery = statusQuery.Or(u => u.Status == temp);
+                }
+
+                query = query.And(statusQuery);
+            }
+
+            return query;
+        }
+
+    }
+}
